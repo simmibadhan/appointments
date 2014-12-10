61bd507fff59c36294610e16ed1ebce987e51750
Simmi Badhan <simmi@addvalsolutions.com>
2014-12-10 17:43:03 +0530

    project rename

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_ice_cube_app_session'
+Rails.application.config.session_store :cookie_store, key: '_appointments_session'
